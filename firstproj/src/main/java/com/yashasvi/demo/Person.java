5e0e5ad8591d03b2a97dd8be33a991c704406b6c
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-13 18:04:33 +0530

    created object using autowired

--- firstproj/src/main/java/com/yashasvi/demo/Person.java
+++ firstproj/src/main/java/com/yashasvi/demo/Person.java
@@ -1,12 +1,23 @@
+/*
+ * we can use special annotation autowired which means 
+ * this laptop object will be created  by spring.
+ */
 package com.yashasvi.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
 
+	@Autowired
+	Laptop laptop;
+	
+	
 	public void Code()
 	{
-		System.out.print("i am coding!");
+		laptop.compile();
+		
+		//System.out.print("i am coding!");
 	}
 }
