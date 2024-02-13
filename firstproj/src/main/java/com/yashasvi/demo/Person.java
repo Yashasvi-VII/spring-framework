daf16fc403d698c220177cc07cc83a5c4c40a9a9
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-13 17:57:03 +0530

    created object of class using spring

---
+++ firstproj/src/main/java/com/yashasvi/demo/Person.java
@@ -0,0 +1,12 @@
+package com.yashasvi.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+	public void Code()
+	{
+		System.out.print("i am coding!");
+	}
+}
