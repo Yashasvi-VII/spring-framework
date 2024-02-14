13a4dbcf3500903ff0d3a5271fa9f345e8c2ef91
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-14 17:41:53 +0530

    singleton and prototype

---
+++ beanFactory/BeanFactoryDemo/src/main/java/com/yashasvi/BeanFactoryDemo/App.java
@@ -0,0 +1,48 @@
+/*
+ * spring creates object even if we don't ask 
+ * application context will simply create spring container for you, all the process happens in 
+ * jvm and jvm will have a special container called spring container which will have spring beans
+ * 
+ * any class that has certain variables and every variable will have getters and setters is normally 
+ * refereed as bean.
+ * 
+ * spring not create new object it will give a new reference , see obj1 and obj2 both are referring to 
+ * same object.
+ * 
+ * so we normally call all the beans as singleton beans. (object will be created only once)
+ * 
+ * To create multiple objects , we can do that using scopes in spring xml bean tag , by default 
+ * scope is singleton, for multiple scope use prototype
+ * 
+ * prototype mean spring container will give new instance when ever we ask
+ * 
+ * but in singleton even if object is not asked spring will make object but prototype will not create
+ *object when not asked.
+ * 
+ */
+package com.yashasvi.BeanFactoryDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+
+		// FileSystemXmlApplicationContext context = new
+		// FileSystemXmlApplicationContext("spring.xml");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		// BeanFactory factory = new BeanFactory(new FileSystemResource("spring.xml"));
+
+		Person obj1 = (Person) context.getBean("person");
+
+		obj1.Code();
+		obj1.age = 15;
+		System.out.println(obj1.age);
+
+		Person obj2 = (Person) context.getBean("person");
+
+		obj2.Code();
+		System.out.println(obj2.age);
+	}
+}
