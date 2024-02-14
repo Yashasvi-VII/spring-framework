ea57ded99df6b428ab97944c9dd840b2ab0f52f6
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-14 17:17:54 +0530

    created object using application context

--- beanFactory/BeanFactoryDemo/src/main/java/com/yashasvi/BeanFactoryDemo/App.java
+++ beanFactory/BeanFactoryDemo/src/main/java/com/yashasvi/BeanFactoryDemo/App.java
@@ -1,27 +1,19 @@
 /*
- * 
-It seems like you're trying to instantiate a BeanFactory object using new,
- but the BeanFactory class might be abstract or might not have a public constructor.
-  Instead, you should use a factory method or a builder to create an instance of BeanFactory.
-   If you're using Spring Framework, you typically don't directly instantiate BeanFactory like this.
-    Instead, you would use the ApplicationContext interface or its implementations,
-     such as ClassPathXmlApplicationContext or FileSystemXmlApplicationContext,
-      to load and manage your beans.
-
-Here's how you can do it using FileSystemXmlApplicationContext:
-
-java
-
+ * We are now using application contex
+ * we have to move the spring.xml file in class path
  */
 package com.yashasvi.BeanFactoryDemo;
 
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
 
-		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("spring.xml");
+		// FileSystemXmlApplicationContext context = new
+		// FileSystemXmlApplicationContext("spring.xml");
 
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		// BeanFactory factory = new BeanFactory(new FileSystemResource("spring.xml"));
 
 		Person obj = (Person) context.getBean("person");
