5f9940fc341e7162f8581780f6d7b3d14213d44e
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-18 00:11:35 +0530

    Request param

--- springmvc/src/main/java/com/yashasvi/springmvcboot/HomeController.java
+++ springmvc/src/main/java/com/yashasvi/springmvcboot/HomeController.java
@@ -1,22 +1,13 @@
 /*
- * In servlet when we need to take input from the user we use a special interface 
- * HttpServletRequest using that we can fetch values and HttpServletResponse to send the data.
- * 
- * In servlet we use concept of http session using which we can add this thirdValue in that object and in jsp
- * we can fetch it or we can also use send dispatcher servlet where we can pass the value as "result.jsp?num3="+num3 which is 
- * called as url rewriting
- * 
- * Here we are using Http Session 
- * 
- * in result.jsp we can use java code or use session object to fetch the value or we can use JSTL 
- * 
- * as per JSTL syntax we simply have to use ${name} if it is not working then write isELIgnored="false"
+ * adding Request Param to clean the code!
+ * we are reducing number of lines as we don't need to assign and pass the objects .
 */
 
 package com.yashasvi.springmvcboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -31,14 +22,14 @@ public class HomeController {
 	}
 	
 	@RequestMapping("add")
-	public String add(HttpServletRequest req )
+	public String add(@RequestParam("Number 1") int firstValue,@RequestParam("Number 2") int secondValue ,HttpSession session)
 	{
-		int firstValue = Integer.parseInt(req.getParameter("Number 1"));
-		int secondValue = Integer.parseInt(req.getParameter("Number 2"));
-		
+//		int firstValue = Integer.parseInt(req.getParameter("Number 1"));
+//		int secondValue = Integer.parseInt(req.getParameter("Number 2"));
+//		
 		int thirdValue = firstValue + secondValue;
 		
-		HttpSession session =  req.getSession();
+	//	HttpSession session =  req.getSession();
 		
 		session.setAttribute("thirdValue", thirdValue);
 		
