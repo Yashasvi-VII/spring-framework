d766046abca21b75a39ec68704b01766a6e0ef72
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-18 00:19:39 +0530

    model and view

--- springmvc/src/main/java/com/yashasvi/springmvcboot/HomeController.java
+++ springmvc/src/main/java/com/yashasvi/springmvcboot/HomeController.java
@@ -1,6 +1,14 @@
 /*
- * adding Request Param to clean the code!
- * we are reducing number of lines as we don't need to assign and pass the objects .
+ * Concept of model and view 
+ * 
+ * we can remove the HttpSession using model and view
+ * 
+ * model will have data thirdValue is the data so we just need to put this value in model object and also we need to specify the view 
+ * here view is result.jsp .
+ * 
+ * we have inbuilt class model and view 
+ * 
+ * instead of returning the string we can return the mv and we need to change the return type to model and view 
 */
 
 package com.yashasvi.springmvcboot;
@@ -8,6 +16,7 @@ package com.yashasvi.springmvcboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -22,18 +31,17 @@ public class HomeController {
 	}
 	
 	@RequestMapping("add")
-	public String add(@RequestParam("Number 1") int firstValue,@RequestParam("Number 2") int secondValue ,HttpSession session)
+	public ModelAndView add(@RequestParam("Number 1") int firstValue,@RequestParam("Number 2") int secondValue)
 	{
-//		int firstValue = Integer.parseInt(req.getParameter("Number 1"));
-//		int secondValue = Integer.parseInt(req.getParameter("Number 2"));
-//		
+	
+		ModelAndView mv = new ModelAndView();
+		
 		int thirdValue = firstValue + secondValue;
 		
-	//	HttpSession session =  req.getSession();
+		// session.setAttribute("thirdValue", thirdValue);
+		mv.addObject("thirdValue", thirdValue);
+		mv.setViewName("result.jsp");
 		
-		session.setAttribute("thirdValue", thirdValue);
-		
-		
-		return "result.jsp";
+		return mv;
 	}
 }
