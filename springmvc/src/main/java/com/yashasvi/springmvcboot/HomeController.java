ee8f405ab7a8321f54f87c78c4b2a836d9fe6d5e
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-18 11:46:48 +0530

    used model interface instead of model and view

--- springmvc/src/main/java/com/yashasvi/springmvcboot/HomeController.java
+++ springmvc/src/main/java/com/yashasvi/springmvcboot/HomeController.java
@@ -1,25 +1,13 @@
 /*
- * Concept:- prefix and suffix 
+ * Concept:- model and modelmap
  * 
- * anyone can access webApp files so to secure the files we can keep the files in webIner which is private and can only be accessed 
- * by controller
  * 
- * we have to inform dispatcher servlet that whenever you are looking for the view go to views folder 
- * in application.properties folder.
- * 
- * If we want to assign path and the extension we have to use prefix and suffix 
- * 
- * go to https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html
- * search for spring mvc look for spring.mvc.view.prefix and suffix
- * 
- * prefix is what before result and suffix is what after the result.
- * 
- * so we have removed the .jsp extension from the code and put that another folder
 */
 
 package com.yashasvi.springmvcboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,17 +25,17 @@ public class HomeController {
 	}
 	
 	@RequestMapping("add")
-	public ModelAndView add(@RequestParam("Number 1") int firstValue,@RequestParam("Number 2") int secondValue)
+	public String add(@RequestParam("Number 1") int firstValue,@RequestParam("Number 2") int secondValue,Model m)
 	{
 	
-		ModelAndView mv = new ModelAndView();
+		//ModelAndView mv = new ModelAndView();
 		
 		int thirdValue = firstValue + secondValue;
 		
 		// session.setAttribute("thirdValue", thirdValue);
-		mv.addObject("thirdValue", thirdValue);
-		mv.setViewName("result");
-		
-		return mv;
+	//	mv.addObject("thirdValue", thirdValue);
+//		mv.setViewName("result");
+	m.addAttribute("thirdValue", thirdValue)	;
+		return "result";
 	}
 }
